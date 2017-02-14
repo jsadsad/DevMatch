29ebdb09e0521b6e9e5cabee8abb1ebc58abbdf7
joshsad2@gmail.com <joshsad2@gmail.com>
2017-02-14 08:28:10 +0000

    added hirb gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,19 @@
 class ContactsController < ApplicationController
-<<<<<<< HEAD
-def new
-  @contact = Contact.new
-end
-def create
-  @contact = Contact.new(contact_params)
-  if @contact.save
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
      redirect_to new_contact_path, notice: "Message sent."
   else
      redirect_to new_contact_path, notice: "Error occured."
+    end
   end
-end
-private
-  def contact_params
+  
+  private
+    def contact_params
      params.require(:contact).permit(:name, :email, :comments)
-=======
-  def new
->>>>>>> 38729442c712dfceccdb259184d418a596724c9c
   end
 end
