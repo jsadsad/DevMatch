bfb6c2bc2849fa8c54b19c87c8e1f246176f9326
joshsad2@gmail.com <joshsad2@gmail.com>
2017-04-04 08:07:07 +0000

    Added Before filters to check correct users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   # GET to /users/:id
   # show is what you use if you want to see an individual thing
   def show
